81651d31e2a53b83a737b61e32e9d27ca34bd777
eulerfernandes <pedraeuler@gmail.com>
2025-06-16 20:48:40 -0300

    refactor(conjunto_dados): remover uso do service e consultar dados direto no controller

--- api-medicamentos/src/controllers/conjuntoDadosController.ts
+++ api-medicamentos/src/controllers/conjuntoDadosController.ts
@@ -1,11 +1,14 @@
+// controllers/conjuntoDadosController.ts
+
 import { Request, Response } from "express";
-import { listConjuntos } from "../services/conjuntoDadosService";
+import { db } from "../config/database";
 
 export const getConjuntos = async (req: Request, res: Response) => {
   try {
-    const dados = await listConjuntos();
-    res.status(200).json(dados);
+    const result = await db.query("SELECT * FROM conjunto_dados");
+    res.status(200).json(result.rows);
   } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar os dados." });
+    console.error("Erro ao buscar dados:", error);
+    res.status(500).json({ error: "Erro ao buscar dados" });
   }
 };
